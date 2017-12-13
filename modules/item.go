94c5e7dba9b7f6ab34b5e0eb6b1828c91fe2722d
Pradnya A <anyaprdny@gmail.com>
2017-12-13 11:20:58 +0700

    Database Structure

---
+++ modules/item.go
@@ -0,0 +1,20 @@
+package main
+
+type barang struct {
+	Kode_Barang	string
+	Nama_Barang	string
+	Kondisi		string
+	Lokasi		string
+}
+
+type bidang struct {
+	Nama_Bidang 	string
+	ID_Bidang	string
+}
+
+type inventarisbarang struct {
+	ID_Bidang	string
+	Kode_Barang	string
+}
+
+
